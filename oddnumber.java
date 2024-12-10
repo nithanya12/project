2df7f931cd7c72270539f8da68f5cfc8896d0473
nithanya12 <peralanitanya@gmail.com>
2024-12-10 15:17:22 +0530

    Update oddnumber.java

--- oddnumber.java
+++ oddnumber.java
@@ -5,7 +5,8 @@ public class oddnumbers
 		int number=100;
 		System.out.print("List of odd numbers from 1-100":);
 		for (int i=1;i<number;i++)
-		{if(i%2!=0)
+		{
+			if(i%2!=0)
 			System.out.print(i+ "");
 		}
 	}
